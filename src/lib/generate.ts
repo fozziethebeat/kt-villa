daa124d7fd7541b97e7099f73801fec29e9b432e
Keith Stevens <fozziethebeat@gmail.com>
2024-11-25 21:24:26 -0800

    Sorta adding support for Imagen

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -7,6 +7,7 @@ import {
   paginateListObjectsV2,
   GetObjectCommand,
 } from '@aws-sdk/client-s3';
+import {helpers, PredictionServiceClient} from '@google-cloud/aiplatform';
 import axios from 'axios';
 import ShortUniqueId from 'short-unique-id';
 import Together from 'together-ai';
@@ -160,7 +161,70 @@ class BentoFluxGenerator extends ImageGenerationService {
   }
 }
 
+class GeminiImagenGenerator extends ImageGenerationService {
+  protected projectID: string;
+  protected location: string;
+
+  protected predictionServiceClient: PredictionServiceClient;
+  constructor(bucketName: string, projectID: string, location: string) {
+    super(bucketName);
+
+    this.projectID = projectID;
+    this.location = location;
+    this.predictionServiceClient = new PredictionServiceClient({
+      apiEndpoint: `${location}-aiplatform.googleapis.com`,
+    });
+  }
+
+  async generateImageFromAdapter(itemId, adapterSettings): Promise<string> {
+    const endpoint = `projects/${this.projectId}/locations/${this.location}/publishers/google/models/imagen-3.0-generate-001`;
+    const parameters = helpers.toValue({
+      sampleCount: 1,
+      aspectRatio: '1:1',
+      safetyFilterLevel: 'block_none',
+      personGeneration: 'allow_adult',
+    });
+    const instances = [
+      helpers.toValue({
+        prompt: this.getPrompt(adapterSettings),
+      }),
+    ];
+    const request = {
+      endpoint,
+      instances,
+      parameters,
+    };
+
+    try {
+      const [response] = await this.predictionServiceClient.predict(request);
+      if (!response || response.predictions.length === 0) {
+        throw new Error('No Gemini Response');
+      }
+
+      const prediction = response.predictions[0];
+      const imageBuffer = Buffer.from(
+        prediction.structValue.fields.bytesBase64Encoded.stringValue,
+        'base64',
+      );
+
+      // Save the image to AWS S3
+      const targetKey = `results/${itemId}.png`;
+      return await this.uploadImageToS3(imageBuffer, targetKey);
+    } catch (e) {
+      console.log(e);
+      throw new Error("Gemini Don't work");
+    }
+  }
+}
+
 function imageGeneratorFactory() {
+  if (process.env.GEMINI_PROJECT_ID && process.env.GEMINI_LOCATION) {
+    return new GeminiImagenGenerator(
+      process.env.AWS_BUCKET,
+      process.env.GEMINI_PROJECT_ID,
+      process.env.GEMINI_LOCATION,
+    );
+  }
   if (process.env.TOGETHER_API_KEY && process.env.TOGETHER_API_MODEL) {
     return new TogetherFluxGenerator(
       process.env.AWS_BUCKET,
