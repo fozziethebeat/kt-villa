da124f6ba7d776b8040d61333b3951562f6d7b2e
Keith Stevens <fozziethebeat@gmail.com>
2024-12-14 21:13:34 -0800

    Adding simple example of calling gemini text

--- dream/src/cli.ts
+++ dream/src/cli.ts
@@ -1,3 +1,4 @@
+import {GoogleGenerativeAI} from '@google/generative-ai';
 import {Command} from 'commander';
 
 import {PrismaClient} from '@prisma/client';
@@ -10,5 +11,16 @@ program
   .name('yumegai-tools')
   .description('CLI tools for Yumegai Operations')
   .version('1.0.0');
+
+program
+  .command('generate')
+  .description('Generate Test Generative text response')
+  .option('-p, --prompt <query>')
+  .action(async options => {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({model: process.env.GEMINI_MODEL});
+    const result = await model.generateContent([options.prompt]);
+    console.log(result.response.text());
+  });
 
 program.parse();
