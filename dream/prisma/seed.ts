dfd319345e8ff0e28629a1ea3b67223d661f91fd
Keith Stevens <fozziethebeat@gmail.com>
2025-01-18 21:39:47 -0800

    Cleaning up seed data

--- dream/prisma/seed.ts
+++ dream/prisma/seed.ts
@@ -15,21 +15,6 @@ async function devSeed() {
           email: 'fozziethebeat@gmail.com',
           roles: 'admin',
         },
-        {
-          name: 'Friend 1',
-          email: 'fozziethebeat+u1@gmail.com',
-          roles: 'general',
-        },
-        {
-          name: 'Friend 2',
-          email: 'fozziethebeat+u2@gmail.com',
-          roles: 'general',
-        },
-        {
-          name: 'Friend 3',
-          email: 'fozziethebeat+u3@gmail.com',
-          roles: 'general',
-        },
       ],
     });
     await db.magicCode.createMany({
@@ -40,34 +25,6 @@ async function devSeed() {
     });
     await db.dreamTheme.createMany({
       data: dreamThemes,
-    });
-    await db.dream.createMany({
-      data: [
-        {
-          memory: 'm1',
-          story: 's1',
-          dreamImage:
-            'https://stablesoaps-w1.s3.amazonaws.com/results/vfuX7I.png',
-          prompt: 'p1',
-          userId: users[1].id,
-        },
-        {
-          memory: 'm2',
-          story: 's2',
-          dreamImage:
-            'https://stablesoaps-w1.s3.amazonaws.com/results/vfuX7I.png',
-          prompt: 'p2',
-          userId: users[2].id,
-        },
-        {
-          memory: 'm3',
-          story: 's3',
-          dreamImage:
-            'https://stablesoaps-w1.s3.amazonaws.com/results/vfuX7I.png',
-          prompt: 'p3',
-          userId: users[3].id,
-        },
-      ],
     });
     await db.promptTemplate.createMany({
       data: [
@@ -106,21 +63,8 @@ Make sure the image prompt fits the following artistic style: {{ style }}`,
   }
 }
 
-async function prodSeed() {
-  try {
-  } catch (error) {
-    console.warn('Please define your seed data.');
-    console.error(error);
-  }
-}
-
 async function main() {
-  const environment = process.env.NODE_ENV || 'dev';
-  if (environment === 'prod') {
-    await prodSeed();
-  } else {
-    await devSeed();
-  }
+  await devSeed();
 }
 
 main()
