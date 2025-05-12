5b924d5e2fff5e22b1c7e6ad09350c1ccee45d00
Keith Stevens <fozziethebeat@gmail.com>
2025-05-11 18:05:24 -0700

    Adding dump and restore cli tools

--- dream/src/cli.ts
+++ dream/src/cli.ts
@@ -36,4 +36,66 @@ program
     console.log(url);
   });
 
+program
+  .command('db-dump')
+  .description('Dump all contents of the database into a JSON file')
+  .option('-o, --output <path>')
+  .action(async options => {
+    const dreamThemes = await db.dreamTheme.findMany();
+    const promptTemplates = await db.promptTemplate.findMany();
+    const styles = await db.style.findMany();
+    const dreams = await db.dream.findMany();
+    const magicCodes = await db.magicCode.findMany();
+    const accounts = await db.account.findMany();
+    const user = await db.user.findMany();
+    const allData = {
+      dreamThemes,
+      promptTemplates,
+      styles,
+      dreams,
+      magicCodes,
+      accounts,
+      user,
+    };
+    fs.writeFileSync(options.output, JSON.stringify(allData, null, 2));
+  });
+
+program
+  .command('db-restore')
+  .description('Restore all contents of the database from a JSON file')
+  .option('-i, --input <path>')
+  .action(async options => {
+    const allData = JSON.parse(fs.readFileSync(options.input).toString());
+    const {
+      dreamThemes,
+      promptTemplates,
+      styles,
+      dreams,
+      magicCodes,
+      accounts,
+      user,
+    } = allData;
+    for (const data of user) {
+      await db.user.create({data});
+    }
+    for (const data of accounts) {
+      await db.account.create({data});
+    }
+    for (const data of magicCodes) {
+      await db.magicCode.create({data});
+    }
+    for (const data of dreams) {
+      await db.dream.create({data});
+    }
+    for (const data of styles) {
+      await db.style.create({data});
+    }
+    for (const data of promptTemplates) {
+      await db.promptTemplate.create({data});
+    }
+    for (const data of dreamThemes) {
+      await db.dreamTheme.create({data});
+    }
+  });
+
 program.parse();
